8d2cab091934322aae72b9ff157a6935a563546c
imjahan <55078995+imjahan@users.noreply.github.com>
2021-05-07 18:37:49 +0530

    Add files via upload

---
+++ extrenal.js
@@ -0,0 +1,153 @@
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
+
+
+/*smoth scroling*/
+
+$(document).ready(function(){
+  $('.menu a').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 2000);
+       return false;
+      }
+    }
+  });
+});
+
+/*scroll to top*/
+$(document).ready(function(){
+$(window).scroll(function(){
+    if($(this).scrollTop() > 200) {
+        $("#arrow i").fadeIn();
+}
+    else {
+        $("#arrow i").fadeOut();
+}
+
+});
+$("#arrow i").on('click',function(){
+$("html,body").animate({
+    scrollTop: 0
+}, 600);
+    return false;
+});
+
+});
+
+//fixed header
+
+jQuery(window).scroll(function(){
+    if (jQuery(window).scrollTop() >= 10) {
+       jQuery('#home').addClass('fixed-header');
+    }
+    else {
+       jQuery('#home').removeClass('fixed-header');
+    }
+});
+
+// menu toggle 
+
+
+$(document).ready(function(){
+$(".menu-toggle").click(function(){
+$(".menu-toggle").toggleClass("tog-active");
+$(".menu").toggleClass("tog-active");
+});
+});
+
+
+//counter
+
+
+
+$(document).ready(function($) {
+    $(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() == $(document).height()) {
+      // alert("bottom!");//
+      counter();
+       }
+});
+    $( "#counterstart" ).on( "click", counter );
+    function counter(){
+        "use strict";
+        function count($this){
+        var current = parseInt($this.html(),10);
+        current = current + 1; /* Where 50 is increment */  
+        $this.html(++current);
+            if(current > $this.data('count')){
+                $this.html($this.data('count'));
+            } else {    
+                setTimeout(function(){count($this)}, 70);
+            }
+        }           
+        $(".stat-count").each(function() {
+          $(this).data('count', parseInt($(this).html(), 10));
+          $(this).html('0');
+          count($(this));
+        });
+    }
+        
+   })(jQuery);
+
+   //counter finish //
